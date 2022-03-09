4781a78451e39c5f3893f4b87705dded4b765fcb
kid7258 <kid7258@naver.com>
2022-03-09 13:05:57 +0900

    JUnit 5 테스트 이름 표시

--- src/test/java/me/kkkong/infleanthejavatest/StudyTest.java
+++ src/test/java/me/kkkong/infleanthejavatest/StudyTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) // 메서드명의 언더스코어를 띄어쓰기로 변경
 class StudyTest {
 
     @Test
+    @DisplayName("스터디 생성")
     void create() {
         Study study = new Study();
         assertNotNull(study);
