4ee880c3a8c6b3f7fcb883a089073cff8afbb88b
kid7258 <kid7258@naver.com>
2022-04-07 21:36:26 +0900

    Mock 객체 Stubbing

--- src/test/java/me/kkkong/infleanthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/kkkong/infleanthejavatest/study/StudyServiceTest.java
@@ -1,11 +1,18 @@
 package me.kkkong.infleanthejavatest.study;
 
+import me.kkkong.infleanthejavatest.domain.Member;
 import me.kkkong.infleanthejavatest.member.MemberService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class StudyServiceTest {
@@ -19,6 +26,23 @@ public class StudyServiceTest {
     @Test
     void createStudyService() {
         StudyService studyService = new StudyService(memberService, studyRepository);
-        Assertions.assertNotNull(studyService);
+        assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("kid7258@naver.com");
+
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))
+                .thenThrow(new RuntimeException())
+                .thenReturn(Optional.empty());
+
+        assertEquals("kid7258@naver.com", memberService.findById(1L).get().getEmail());
+
+        assertThrows(RuntimeException.class, () -> {
+            memberService.findById(2L);
+        });
+
+        assertEquals(Optional.empty(), memberService.findById(3L));
     }
 }
